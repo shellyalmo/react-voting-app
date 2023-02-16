736ecb42089f8c1a7e9cee2c35b3845ed9743918
shellyalmo <shellyalmo@gmail.com>
2023-02-16 19:10:49 +0200

    set up and organize folders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Login from "./pages/Login.page";
+import "./styles/App.css";
+
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Login></Login>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Login.page.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Wrapper from "../styles/styled/Login.styled";
+
+const Login = () => {
+  return (
+    <Wrapper>
+      <form>
+        <input type="text" name="" value="" />
+        <input type="email" name="" value="" />
+        <input type="password" name="" value="" />
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Login;
