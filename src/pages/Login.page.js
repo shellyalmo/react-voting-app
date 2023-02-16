2325a5223683f1a00a8c27bc9b981c538badab5f
shellyalmo <shellyalmo@gmail.com>
2023-02-16 22:13:50 +0200

    form

--- src/pages/Login.page.js
+++ src/pages/Login.page.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Wrapper from "../styles/styled/Login.styled";
+import votingLogo from "../assets/images/voting-logo.png";
 const Login = () => {
   return (
     <Wrapper>
-      <img src="../assets/images/voting-logo.png" alt="Voting App Logo" />
+      <img src={votingLogo} alt="Voting App Logo" />
       <h1>Voting App</h1>
-      <form>
-        {/* <input type="text" name="" value="" />
-        <input type="email" name="" value="" />
-        <input type="password" name="" value="" /> */}
-      </form>
+      <Form setParentState={console.log} />
     </Wrapper>
+  );
+};
+
+const Form = (props) => {
+  const [username, setusername] = useState("");
+  const [email, setemail] = useState("");
+  // callback func from App - setData
+  const setParentState = props.setParentState;
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const formData = {
+          username,
+          email,
+        };
+        setParentState(formData);
+      }}
+    >
+      <label htmlFor="userName">User Name: </label>
+      <input
+        type="text"
+        name="userName"
+        value={username}
+        onChange={(e) => {
+          setusername(e.target.value);
+        }}
+      />
+      <br />
+      <label htmlFor="email">Email: </label>
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => {
+          setemail(e.target.value);
+        }}
+      />
+      <button type="submit">Continue</button>
+    </form>
   );
 };
 
