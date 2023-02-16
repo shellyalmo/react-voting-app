440a9eac696d9f5fcaf310a806ebcaadacd8917d
shellyalmo <shellyalmo@gmail.com>
2023-02-16 21:12:13 +0200

    cleanup

--- src/pages/Login.page.js
+++ src/pages/Login.page.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Wrapper from "../styles/styled/Login.styled";
-
 const Login = () => {
   return (
     <Wrapper>
+      <img src="../assets/images/voting-logo.png" alt="Voting App Logo" />
+      <h1>Voting App</h1>
       <form>
-        <input type="text" name="" value="" />
+        {/* <input type="text" name="" value="" />
         <input type="email" name="" value="" />
-        <input type="password" name="" value="" />
+        <input type="password" name="" value="" /> */}
       </form>
     </Wrapper>
   );
