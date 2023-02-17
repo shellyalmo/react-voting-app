de6178e419484b4a29a3ff20dca9f5cf553dc4bd
shellyalmo <shellyalmo@gmail.com>
2023-02-17 11:41:03 +0200

    fix:

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { checkUserInDb } from "./api/login";
 import Login from "./pages/Login.page";
 import "./styles/App.css";
