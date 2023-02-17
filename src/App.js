cda001d012846da862a71d929cb1db4a0700ca06
shellyalmo <shellyalmo@gmail.com>
2023-02-17 09:47:43 +0200

    feat: login page update App parent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React, { useState } from "react";
 import Login from "./pages/Login.page";
 import "./styles/App.css";
 
 function App() {
+  const [loginData, setloginData] = useState(null);
   return (
     <div className="App">
-      <Login></Login>
+      <Login setLogin={setloginData}></Login>
     </div>
   );
 }
